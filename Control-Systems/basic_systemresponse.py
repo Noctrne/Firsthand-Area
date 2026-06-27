9233e59563769d88075ab5015618492aeffa53fe
Noctrne <dhiipt32@gmail.com>
2026-06-28 00:36:48 +0700

    plot ct

--- Control-Systems/basic_systemresponse.py
+++ Control-Systems/basic_systemresponse.py
@@ -3,7 +3,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def main():
-    print("test commit")
+    num = [1]
+    den = [1, 3, 2]
+    system = ct.TransferFunction(num, den)
+
+    t = np.linspace(0, 10, 1001)
+
+    t_out, y_out = ct.step_response(system, T=t)
+
+    plt.figure()
+    plt.plot(t_out, y_out)
+    plt.title('Step Response of the System')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Output')
+    plt.grid()
+    plt.show()
 
 
 
